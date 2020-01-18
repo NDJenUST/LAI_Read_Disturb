0bf72f01954a598d7c016941900ff40cd51095d3
Keven1 <410121022@gms.ndhu.edu.tw>
2020-01-18 12:41:49 +0800

    Add files via upload
    on net

---
+++ main.py
@@ -0,0 +1,14 @@
+def main() :
+    '''
+    1.Define Module
+    2.import logging
+    3.Decorator
+    4.Flow of all
+    [5.unitest, if some error usually occur]
+    [6. command line interface to get arg... you need]
+    '''
+    print('Enter')
+
+
+if __name__ == '__main__' :
+    main()
